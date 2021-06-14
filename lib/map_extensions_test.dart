3f4bd8b7fd036a566de138d6fe748eca82dbaa81
Spyryto <danielezecchin@gmail.com>
2021-06-14 17:18:13 +0200

    Refactored map extensions tests

--- lib/map_extensions_test.dart
+++ lib/map_extensions_test.dart
@@ -2,24 +2,24 @@ import 'package:test/test.dart';
 
 import 'map_extensions.dart';
 
+bool codeMatchesCharacter(character, asciiCode) =>
+    character.codeUnitAt(0) == asciiCode;
+
 void main() {
   group('any', () {
     test('returns true if all elements satisfy predicate', () {
       var map = {'A': 65, 'B': 66, 'C': 67};
-      var result = map
-          .any((character, asciiCode) => character.codeUnitAt(0) == asciiCode);
+      var result = map.any(codeMatchesCharacter);
       expect(result, true);
     });
     test('returns true if some elements satisfy predicate', () {
       var map = {'A': 65, 'b': 66, 'C': 67};
-      var result = map
-          .any((character, asciiCode) => character.codeUnitAt(0) == asciiCode);
+      var result = map.any(codeMatchesCharacter);
       expect(result, true);
     });
     test('returns false if none of the elements satisfy predicate', () {
       var map = {'a': 65, 'b': 66, 'c': 67};
-      var result = map
-          .any((character, asciiCode) => character.codeUnitAt(0) == asciiCode);
+      var result = map.any(codeMatchesCharacter);
       expect(result, false);
     });
   });
@@ -27,14 +27,12 @@ void main() {
   group('every', () {
     test('returns true if all elements satisfy predicate', () {
       var map = {'A': 65, 'B': 66, 'C': 67};
-      var result = map.every(
-          (character, asciiCode) => character.codeUnitAt(0) == asciiCode);
+      var result = map.every(codeMatchesCharacter);
       expect(result, true);
     });
     test('returns false if not all elements satisfy predicate', () {
       var map = {'A': 65, 'b': 66, 'C': 67};
-      var result = map.every(
-          (character, asciiCode) => character.codeUnitAt(0) == asciiCode);
+      var result = map.every(codeMatchesCharacter);
       expect(result, false);
     });
   });
